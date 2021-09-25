53c6ea9425b31f4afd080dd7ac7167ac376019b3
victorssun <victorssun@hotmail.com>
2021-09-25 02:14:18 -0600

    remove navbar hover

--- navbar_highlight.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    var pagePathName = window.location.pathname;
-    var top_url = pagePathName.split("/")[1];
-
-    if (top_url) {
-      $("#" + top_url).addClass('active');
-    }
-});
